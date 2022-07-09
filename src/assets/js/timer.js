5d0717a20ff7ee42d1f0ee36706ae3fa020595d6
catekui <catherine.wanjiku@student.moringaschool.com>
2022-07-09 22:42:48 +0300

    add timer

--- src/assets/js/timer.js
+++ src/assets/js/timer.js
@@ -1,98 +1,18 @@
-//  const root = document.getElementsByClassName("timer")
+const startTiming = 40
 
- class Timer {
-    
-    constructor(root) {
-      root.innerHTML = Timer.getHTML();
-  
-      this.el = {
-        minutes: root.querySelector(".timer__part--minutes"),
-        seconds: root.querySelector(".timer__part--seconds"),
-        control: root.querySelector(".timer__btn--control"),
-        reset: root.querySelector(".timer__btn--reset")
-      };
-  
-      this.interval = null;
-      this.remainingSeconds = 0;
-  
-      this.el.control.addEventListener("click", () => {
-        if (this.interval === null) {
-          this.start();
-        } else {
-          this.stop();
+        let time = startTiming * 60
+        const count = document.getElementById("countdownTimer")
+        setInterval(updateTimer, 1000)
+        function updateTimer() {
+            const minutes = Math.floor(time / 60)
+
+            let seconds = time % 60
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            count.innerHTML = `${minutes}:${seconds}`
+            time--
         }
-      });
-  
-      this.el.reset.addEventListener("click", () => {
-        const inputMinutes = prompt("Enter number of minutes:");
-  
-        if (inputMinutes < 60) {
-          this.stop();
-          this.remainingSeconds = inputMinutes * 60;
-          this.updateInterfaceTime();
+
+        function play(){
+            var audio = new Audio('http://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3');
+            audio.play();
         }
-      });
-    }
-  
-    updateInterfaceTime() {
-      const minutes = Math.floor(this.remainingSeconds / 60);
-      const seconds = this.remainingSeconds % 60;
-  
-      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
-      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
-    }
-  
-    updateInterfaceControls() {
-      if (this.interval === null) {
-        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-        this.el.control.classList.add("timer__btn--start");
-        this.el.control.classList.remove("timer__btn--stop");
-      } else {
-        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
-        this.el.control.classList.add("timer__btn--stop");
-        this.el.control.classList.remove("timer__btn--start");
-      }
-    }
-  
-    start() {
-      if (this.remainingSeconds === 0) return;
-  
-      this.interval = setInterval(() => {
-        this.remainingSeconds--;
-        this.updateInterfaceTime();
-  
-        if (this.remainingSeconds === 0) {
-          this.stop();
-        }
-      }, 1000);
-  
-      this.updateInterfaceControls();
-    }
-  
-    stop() {
-      clearInterval(this.interval);
-  
-      this.interval = null;
-  
-      this.updateInterfaceControls();
-    }
-    static getHTML() {
-        return `
-                <span class="timer__part timer__part--minutes">00</span>
-                <span class="timer__part">:</span>
-                <span class="timer__part timer__part--seconds">00</span>
-                <button type="button" class="timer__btn timer__btn--control timer__btn--start">
-                    <span class="material-icons">play_arrow</span>
-                </button>
-                <button type="button" class="timer__btn timer__btn--reset">
-                    <span class="material-icons">timer</span>
-                </button>
-            `;
-      }
-      
-  
-    
-  }
-  new Timer(
-    document.querySelector(".timer"))
-  
