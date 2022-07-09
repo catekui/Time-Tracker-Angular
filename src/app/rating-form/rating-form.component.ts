7a05beaaf7f10482930f864a8407b6a9f7cedc3b
davine94 <davine94>
2022-07-09 21:06:25 +0300

    Added the rating form

--- src/app/rating-form/rating-form.component.ts
+++ src/app/rating-form/rating-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-rating-form',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating-form.component.css']
 })
 export class RatingFormComponent implements OnInit {
+  myForm = new FormGroup({
+    efficiency: new FormControl(''),
+    experience: new FormControl(''),
+    productivity: new FormControl(''),
+  });
 
-  constructor() { }
+  title = 'Rate'
+  clearInput(){
+    this.myForm.reset()
+   }
+    constructor() { 
+  
+  
+    }
+  
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
-
+    onSubmit = () => {
+      console.log(this.myForm.value.activity)
+      this.myForm.reset()
+ 
+}
 }
